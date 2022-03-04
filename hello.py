0a54556461d4d82784e019082feded28c3b1d942
zlv <59903881+Lildonk@users.noreply.github.com>
2022-03-04 15:10:21 -0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+app #!/usr/bin/env python3
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, world!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+    = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, world!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+    
